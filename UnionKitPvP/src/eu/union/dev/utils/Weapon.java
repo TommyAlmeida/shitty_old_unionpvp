b5566d2094bca52c227bbd9479e48eb48f13c7b0
Miguel <iogurtepll@gmail.com>
2016-05-24 07:39:16 +0000

    Update Weapon.java

--- UnionKitPvP/src/eu/union/dev/utils/Weapon.java
+++ UnionKitPvP/src/eu/union/dev/utils/Weapon.java
@@ -13,6 +13,7 @@ public enum Weapon {
     DEFAULT_ARROW(Material.ARROW, "§aArrow"),
     GRANDPA_STICK(Material.STICK, "§eGrandPa Cane"),
     PULSAR_SHOCK(Material.MAGMA_CREAM, "§ePulsar Shock"),
+    ICECUBE_ITEM(Material.PACKED_ICE, "§aIceCube Item"),
     EXPLODER_TNT(Material.BOW, "§eExploder TnT"),
     ENDERMAGE_PORTAL(Material.ENDER_PORTAL_FRAME, "§5Endermage Portal"),
     FISHERMAN_ROD(Material.FISHING_ROD, "§eFisherman Rod"),
