4cc13ad9df8dd0c589f60767ccbfe2498c487628
FentisDev <fentisdev@hotmail.com>
2016-05-28 13:16:47 -0300

    Kit Repulsion Desbugado

--- UnionKitPvP/src/eu/union/dev/kits/heroic/Repulsion.java
+++ UnionKitPvP/src/eu/union/dev/kits/heroic/Repulsion.java
@@ -14,6 +14,7 @@ import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.util.Vector;
@@ -38,7 +39,9 @@ public class Repulsion extends Kit implements Listener{
         final Player p = e.getPlayer();
         KitManager km = KitManager.getManager();
         if (p.getItemInHand().getType().toString().contains("_SWORD") &&
-                km.getKitAmIUsing(p,"repulsion")){
+                km.getKitAmIUsing(p,"repulsion") &&
+                (e.getAction() == Action.RIGHT_CLICK_AIR ||
+                e.getAction() == Action.RIGHT_CLICK_BLOCK)){
             if (cooldown.tryUse(p)){
                 new BukkitRunnable() {
                     int i = 0;
