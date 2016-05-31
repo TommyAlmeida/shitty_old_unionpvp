502130bed3c82e44a9a7e94e1eca33fdbd4ea54c
FentisDev <fentisdev@hotmail.com>
2016-05-31 12:32:42 -0300

    Menu de Kits Agora tem paginas para que possamos criar mais kits! (Please!!! Testar antes -')

--- UnionKitPvP/src/eu/union/dev/listeners/menus/KitMenu.java
+++ UnionKitPvP/src/eu/union/dev/listeners/menus/KitMenu.java
@@ -45,15 +45,17 @@ public class KitMenu implements Listener {
 
         e.setCancelled(true);
         Inventory inv = Bukkit.createInventory(null, 6 * 9, "Kits");
-        setItems(p, inv, "player");
+        setItems(p, inv, "player",1);
         p.openInventory(inv);
     }
 
-    void setItems(Player p, Inventory inv, String type) {
+    void setItems(Player p, Inventory inv, String type, int page) {
         KitManager km = KitManager.getManager();
 
         inv.clear();
         int slot = 0;
+        int nextpage = page+1;
+        int backpage = page-1;
         {
             ItemStack item = new ItemStack(Material.SKULL_ITEM, 1, (short) 3);
             SkullMeta sm = (SkullMeta) item.getItemMeta();
@@ -68,59 +70,128 @@ public class KitMenu implements Listener {
             inv.setItem(slot++, createItem(Material.STAINED_GLASS_PANE, 1, (byte) 2, "§dEpic Kits"));
             inv.setItem(slot++, createItem(Material.STAINED_GLASS_PANE, 1, (byte) 1, "§6Heroic Kits"));
             inv.setItem(slot++, createItem(Material.STAINED_GLASS_PANE, 1, (byte) 10, "§5Beast Kits"));
-            inv.setItem(slot++, new Icon(Material.CARPET, "§aNext Page").build());
         }
-        if (type.equalsIgnoreCase("all")) {
+        if (type.contains("all")) {
+            inv.setItem(slot++, new Icon(Material.CARPET, "§aNext Page","§5All Kits §6"+nextpage).build());
+            inv.setItem(0, new Icon(Material.CARPET, "§7Back Page","§5All Kits §6"+backpage).build());
+            int kits = 0;
             for (int i = 0; i < km.getKits().size(); i++) {
-                Icon icon = km.getKits().get(i).getIcon();
-                inv.setItem(slot++, KitLayout.getLayout().design(icon, km.getKits().get(i)));
+                if (kits<=44 && page == 1){
+                    Icon icon = km.getKits().get(i).getIcon();
+                    inv.setItem(slot++, KitLayout.getLayout().design(icon, km.getKits().get(i)));
+                }
+                if (kits>=45 && page == 2){
+                    Icon icon = km.getKits().get(i).getIcon();
+                    inv.setItem(slot++, KitLayout.getLayout().design(icon, km.getKits().get(i)));
+                }
+                kits++;
             }
         }
-        if (type.equalsIgnoreCase("player")) {
+        if (type.contains("player")) {
+            inv.setItem(slot++, new Icon(Material.CARPET, "§aNext Page","§5Your Kits §6"+nextpage).build());
+            inv.setItem(0, new Icon(Material.CARPET, "§7Back Page","§5Your Kits §6"+backpage).build());
+            int kits = 0;
             for (int i = 0; i < km.getKits().size(); i++) {
                 if (p.hasPermission(km.getKits().get(i).getPermission())) {
-                    Icon icon = km.getKits().get(i).getIcon();
-                    inv.setItem(slot++, KitLayout.getLayout().design(icon, km.getKits().get(i)));
+                    if (kits<=44 && page == 1){
+                        Icon icon = km.getKits().get(i).getIcon();
+                        inv.setItem(slot++, KitLayout.getLayout().design(icon, km.getKits().get(i)));
+                    }
+                    if (kits>=45 && page == 2){
+                        Icon icon = km.getKits().get(i).getIcon();
+                        inv.setItem(slot++, KitLayout.getLayout().design(icon, km.getKits().get(i)));
+                    }
+                    kits++;
                 }
             }
         }
-        if (type.equalsIgnoreCase("common")) {
+        if (type.contains("common")) {
+            inv.setItem(slot++, new Icon(Material.CARPET, "§aNext Page","§5Common Kits §6"+nextpage).build());
+            inv.setItem(0, new Icon(Material.CARPET, "§7Back Page","§5Common Kits §6"+backpage).build());
+            int kits = 0;
             for (int i = 0; i < km.getKits().size(); i++) {
                 if (km.getKits().get(i).getRarity() == Kit.Rarity.COMMON) {
-                    Icon icon = km.getKits().get(i).getIcon();
-                    inv.setItem(slot++, KitLayout.getLayout().design(icon, km.getKits().get(i)));
+                    if (kits<=44 && page == 1){
+                        Icon icon = km.getKits().get(i).getIcon();
+                        inv.setItem(slot++, KitLayout.getLayout().design(icon, km.getKits().get(i)));
+                    }
+                    if (kits>=45 && page == 2){
+                        Icon icon = km.getKits().get(i).getIcon();
+                        inv.setItem(slot++, KitLayout.getLayout().design(icon, km.getKits().get(i)));
+                    }
+                    kits++;
                 }
             }
         }
-        if (type.equalsIgnoreCase("rare")) {
+        if (type.contains("rare")) {
+            inv.setItem(slot++, new Icon(Material.CARPET, "§aNext Page","§5Rare Kits §6"+nextpage).build());
+            inv.setItem(0, new Icon(Material.CARPET, "§7Back Page","§5Rare Kits §6"+backpage).build());
+            int kits = 0;
             for (int i = 0; i < km.getKits().size(); i++) {
                 if (km.getKits().get(i).getRarity() == Kit.Rarity.RARE) {
-                    Icon icon = km.getKits().get(i).getIcon();
-                    inv.setItem(slot++, KitLayout.getLayout().design(icon, km.getKits().get(i)));
+                    if (kits<=44 && page == 1){
+                        Icon icon = km.getKits().get(i).getIcon();
+                        inv.setItem(slot++, KitLayout.getLayout().design(icon, km.getKits().get(i)));
+                    }
+                    if (kits>=45 && page == 2){
+                        Icon icon = km.getKits().get(i).getIcon();
+                        inv.setItem(slot++, KitLayout.getLayout().design(icon, km.getKits().get(i)));
+                    }
+                    kits++;
                 }
             }
         }
-        if (type.equalsIgnoreCase("epic")) {
+        if (type.contains("epic")) {
+            inv.setItem(slot++, new Icon(Material.CARPET, "§aNext Page","§5Epic Kits §6"+nextpage).build());
+            inv.setItem(0, new Icon(Material.CARPET, "§7Back Page","§5Epic Kits §6"+backpage).build());
+            int kits = 0;
             for (int i = 0; i < km.getKits().size(); i++) {
                 if (km.getKits().get(i).getRarity() == Kit.Rarity.EPIC) {
-                    Icon icon = km.getKits().get(i).getIcon();
-                    inv.setItem(slot++, KitLayout.getLayout().design(icon, km.getKits().get(i)));
+                    if (kits<=44 && page == 1){
+                        Icon icon = km.getKits().get(i).getIcon();
+                        inv.setItem(slot++, KitLayout.getLayout().design(icon, km.getKits().get(i)));
+                    }
+                    if (kits>=45 && page == 2){
+                        Icon icon = km.getKits().get(i).getIcon();
+                        inv.setItem(slot++, KitLayout.getLayout().design(icon, km.getKits().get(i)));
+                    }
+                    kits++;
                 }
             }
         }
-        if (type.equalsIgnoreCase("heroic")) {
+        if (type.contains("heroic")) {
+            inv.setItem(slot++, new Icon(Material.CARPET, "§aNext Page","§5Heroic Kits §6"+nextpage).build());
+            inv.setItem(0, new Icon(Material.CARPET, "§7Back Page","§5Heroic Kits §6"+backpage).build());
+            int kits = 0;
             for (int i = 0; i < km.getKits().size(); i++) {
                 if (km.getKits().get(i).getRarity() == Kit.Rarity.HEROIC) {
-                    Icon icon = km.getKits().get(i).getIcon();
-                    inv.setItem(slot++, KitLayout.getLayout().design(icon, km.getKits().get(i)));
+                    if (kits<=44 && page == 1){
+                        Icon icon = km.getKits().get(i).getIcon();
+                        inv.setItem(slot++, KitLayout.getLayout().design(icon, km.getKits().get(i)));
+                    }
+                    if (kits>=45 && page == 2){
+                        Icon icon = km.getKits().get(i).getIcon();
+                        inv.setItem(slot++, KitLayout.getLayout().design(icon, km.getKits().get(i)));
+                    }
+                    kits++;
                 }
             }
         }
-        if (type.equalsIgnoreCase("beast")) {
+        if (type.contains("beast")) {
+            inv.setItem(slot++, new Icon(Material.CARPET, "§aNext Page","§5Beast Kits §6"+nextpage).build());
+            inv.setItem(0, new Icon(Material.CARPET, "§7Back Page","§5Beast Kits §6"+backpage).build());
+            int kits = 0;
             for (int i = 0; i < km.getKits().size(); i++) {
                 if (km.getKits().get(i).getRarity() == Kit.Rarity.BEAST) {
-                    Icon icon = km.getKits().get(i).getIcon();
-                    inv.setItem(slot++, KitLayout.getLayout().design(icon, km.getKits().get(i)));
+                    if (kits<=44 && page == 1){
+                        Icon icon = km.getKits().get(i).getIcon();
+                        inv.setItem(slot++, KitLayout.getLayout().design(icon, km.getKits().get(i)));
+                    }
+                    if (kits>=45 && page == 2){
+                        Icon icon = km.getKits().get(i).getIcon();
+                        inv.setItem(slot++, KitLayout.getLayout().design(icon, km.getKits().get(i)));
+                    }
+                    kits++;
                 }
             }
         }
@@ -142,41 +213,123 @@ public class KitMenu implements Listener {
             }
             switch (e.getSlot()) {
                 case 0: //Menu Anterior
-                    //Voltar ao menu anterior de kits
-                    e.getView().close();
+                    String nome = item.getItemMeta().getLore().get(0);
+                    if (nome.contains("§5All Kits §6")){
+                        int page = Integer.parseInt(nome.replace("§5All Kits §6",""));
+                        if (page >=1){
+                            setItems(p, e.getClickedInventory(), "all",page);
+                        }
+                    }
+                    if (nome.contains("§5Your Kits §6")){
+                        int page = Integer.parseInt(nome.replace("§5Your Kits §6",""));
+                        if (page >=1){
+                            setItems(p, e.getClickedInventory(), "player",page);
+                        }
+                    }
+                    if (nome.contains("§5Common Kits §6")){
+                        int page = Integer.parseInt(nome.replace("§5Common Kits §6",""));
+                        if (page >=1){
+                            setItems(p, e.getClickedInventory(), "common",page);
+                        }
+                    }
+                    if (nome.contains("§5Rare Kits §6")){
+                        int page = Integer.parseInt(nome.replace("§5Rare Kits §6",""));
+                        if (page >=1){
+                            setItems(p, e.getClickedInventory(), "rare",page);
+                        }
+                    }
+                    if (nome.contains("§5Epic Kits §6")){
+                        int page = Integer.parseInt(nome.replace("§5Epic Kits §6",""));
+                        if (page >=1){
+                            setItems(p, e.getClickedInventory(), "epic",page);
+                        }
+                    }
+                    if (nome.contains("§5Heroic Kits §6")){
+                        int page = Integer.parseInt(nome.replace("§5Heroic Kits §6",""));
+                        if (page >=1){
+                            setItems(p, e.getClickedInventory(), "heroic",page);
+                        }
+                    }
+                    if (nome.contains("§5Beast Kits §6")){
+                        int page = Integer.parseInt(nome.replace("§5Beast Kits §6",""));
+                        if (page >=1){
+                            setItems(p, e.getClickedInventory(), "beast",page);
+                        }
+                    }
                     e.setCancelled(true);
                     break;
                 case 1: //Seus Kits
                     e.setCancelled(true);
-                    setItems(p, e.getClickedInventory(), "player");
+                    setItems(p, e.getClickedInventory(), "player",1);
                     break;
                 case 2: //Todos os Kits
                     e.setCancelled(true);
-                    setItems(p, e.getClickedInventory(), "all");
+                    setItems(p, e.getClickedInventory(), "all",1);
                     break;
                 case 3: //Kits Commons
                     e.setCancelled(true);
-                    setItems(p, e.getClickedInventory(), "common");
+                    setItems(p, e.getClickedInventory(), "common",1);
                     break;
                 case 4: //Kits Rare
                     e.setCancelled(true);
-                    setItems(p, e.getClickedInventory(), "rare");
+                    setItems(p, e.getClickedInventory(), "rare",1);
                     break;
                 case 5: //Kits Epic
                     e.setCancelled(true);
-                    setItems(p, e.getClickedInventory(), "epic");
+                    setItems(p, e.getClickedInventory(), "epic",1);
                     break;
                 case 6: //Kits Heroic
                     e.setCancelled(true);
-                    setItems(p, e.getClickedInventory(), "heroic");
+                    setItems(p, e.getClickedInventory(), "heroic",1);
                     break;
                 case 7: //Kits Beast
                     e.setCancelled(true);
-                    setItems(p, e.getClickedInventory(), "beast");
+                    setItems(p, e.getClickedInventory(), "beast",1);
                     break;
                 case 8: //Next Page
-                    //Abrir o menu para a proxima pagina
-                    e.getView().close();
+                    String name = item.getItemMeta().getLore().get(0);
+                    if (name.contains("§5All Kits §6")){
+                        int page = Integer.parseInt(name.replace("§5All Kits §6",""));
+                        if (page <=2){
+                            setItems(p, e.getClickedInventory(), "all",page);
+                        }
+                    }
+                    if (name.contains("§5Your Kits §6")){
+                        int page = Integer.parseInt(name.replace("§5Your Kits §6",""));
+                        if (page <=2){
+                            setItems(p, e.getClickedInventory(), "player",page);
+                        }
+                    }
+                    if (name.contains("§5Common Kits §6")){
+                        int page = Integer.parseInt(name.replace("§5Common Kits §6",""));
+                        if (page <=2){
+                            setItems(p, e.getClickedInventory(), "common",page);
+                        }
+                    }
+                    if (name.contains("§5Rare Kits §6")){
+                        int page = Integer.parseInt(name.replace("§5Rare Kits §6",""));
+                        if (page <=2){
+                            setItems(p, e.getClickedInventory(), "rare",page);
+                        }
+                    }
+                    if (name.contains("§5Epic Kits §6")){
+                        int page = Integer.parseInt(name.replace("§5Epic Kits §6",""));
+                        if (page <=2){
+                            setItems(p, e.getClickedInventory(), "epic",page);
+                        }
+                    }
+                    if (name.contains("§5Heroic Kits §6")){
+                        int page = Integer.parseInt(name.replace("§5Heroic Kits §6",""));
+                        if (page <=2){
+                            setItems(p, e.getClickedInventory(), "heroic",page);
+                        }
+                    }
+                    if (name.contains("§5Beast Kits §6")){
+                        int page = Integer.parseInt(name.replace("§5Beast Kits §6",""));
+                        if (page <=2){
+                            setItems(p, e.getClickedInventory(), "beast",page);
+                        }
+                    }
                     e.setCancelled(true);
                     break;
             }
