1d1b5daadc2cecaac90794417df358eab0c2bb3e
FentisDev <fentisdev@hotmail.com>
2016-06-18 11:31:12 -0300

    /Coins agora esta desbugado apenas para staff

--- UnionKitPvP/src/eu/union/dev/commands/staff/CoinsCMD.java
+++ UnionKitPvP/src/eu/union/dev/commands/staff/CoinsCMD.java
@@ -3,6 +3,7 @@ package eu.union.dev.commands.staff;
 import eu.union.dev.engine.KPlayer;
 import eu.union.dev.engine.managers.PlayerManager;
 import eu.union.dev.utils.globals.Messages;
+import eu.union.dev.utils.globals.Perms;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -13,16 +14,15 @@ public class CoinsCMD implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-
-
         if (!(sender instanceof Player)) {
             return true; //Retorna
         }
-
         Player player = (Player) sender;
+        if (!Perms.isStaff(player)){
+            return true;
+        }
         Player target = Bukkit.getPlayer(args[0]);
         KPlayer kPlayer = PlayerManager.getPlayer(target.getUniqueId());
-
         if (args.length < 1) {
             player.sendMessage(Messages.PREFIX.toString() + " §cUse: /coins <player> <amount>");
             return true;
@@ -33,10 +33,6 @@ public class CoinsCMD implements CommandExecutor {
         }
 
         kPlayer.addCoins(Long.parseLong(args[1]));
-
-
-
-
 
         return true;
 
